6472a98407a6ac73a366bbf858838181d390ff1f
unknown <singhchauhangautam@gmail.com>
2024-04-11 16:50:37 +0530

    sliceAndSplice

---
+++ slice.js
@@ -0,0 +1,11 @@
+// Slice in Js
+
+
+let arr  =['Ant','Bat','Cat','Dog'];
+let slicearr = arr.slice(0,-2);
+console.log(slicearr);
+
+// splice in js 
+let arr1 =[1,2,3,2,3];
+let newslice = arr1.splice(0,1,44);
+console.log(arr1)
